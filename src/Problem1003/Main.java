482f982939a21ded1cfacc3d8a6cc110ef74b926
Mateus Gabi <mateusgabimoreira@hotmail.com>
2016-07-25 12:56:32 -0400

    Problem 1003

---
+++ src/Problem1003/Main.java
@@ -0,0 +1,25 @@
+package Problem1003;
+
+import java.io.IOException;
+import java.util.Scanner;
+
+/**
+ *
+ * @author Mateus Gabi
+ */
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        
+        Scanner s = new Scanner(System.in);
+        
+        int A = s.nextInt();
+        int B = s.nextInt();
+        
+        int SOMA = A + B;
+        
+        System.out.printf("SOMA = %d\n", SOMA);        
+        
+    }
+    
+}
