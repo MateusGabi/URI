71a162a3ae20d0d7b8576ab1cf5aeb6cc3386afa
Mateus Gabi <mateusgabimoreira@hotmail.com>
2016-07-24 21:17:54 -0400

    second commit

---
+++ src/Problem1001/Main.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Problem1001;
+
+import java.io.IOException;
+import java.util.Scanner;
+
+/**
+ *
+ * @author Elton
+ */
+public class Main {
+     public static void main(String[] args) throws IOException {
+ 
+        /**
+         * Escreva a sua solução aqui
+         * Code your solution here
+         * Escriba su solución aquí
+         */
+         
+         Scanner s = new Scanner(System.in);
+         
+         int A = s.nextInt();
+         int B = s.nextInt();
+         
+         int X = A + B;
+         
+         System.out.println("X = "+ X);
+    } 
+}
